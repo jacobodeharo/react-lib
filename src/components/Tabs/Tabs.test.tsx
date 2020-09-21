565382520eae39e2134c8f5fbed9c145a10f57b2
Jacobo de Haro <jacobodeharo@gmail.com>
2020-09-21 23:21:46 +0200

    Add Tabs component unit and snapshot tests

---
+++ src/components/Tabs/Tabs.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import { renderHook } from "@testing-library/react-hooks";
+import { Tabs } from './Tabs';
+import { DARK_THEME } from '../theme/variants/darkTheme';
+import { Selector } from "./Selector/Selector";
+import { Panel } from "./Panel/Panel";
+import { useTabs } from "./Tabs.hooks";
+import { TabsContext } from "./Tabs.context";
+
+describe('Tabs', () => {
+  const PANEL_CHILDREN = 'Panel Label';
+  const SELECTOR_CHILDREN = 'SelectorLabel';
+  afterEach(cleanup);
+
+  test('has to be rendered', () => {
+    render(
+      <Tabs selectedTab='tabA'>
+        <Selector tabId='tabA'>{SELECTOR_CHILDREN}</Selector>
+        <Panel tabId='tabA'>{PANEL_CHILDREN}</Panel>
+      </Tabs>
+    );
+    const PanelElement = screen.getByText(PANEL_CHILDREN);
+    expect(PanelElement).toBeInTheDocument();
+    const SelectorElement = screen.getByText(SELECTOR_CHILDREN);
+    expect(SelectorElement).toBeInTheDocument();
+    expect(SelectorElement).toMatchSnapshot();
+  });
+
+  test('has to accept theming', () => {
+    render(
+      <Tabs selectedTab='tabA' theme={DARK_THEME}>
+        <Selector tabId='tabA'>{SELECTOR_CHILDREN}</Selector>
+        <Panel tabId='tabA'>{PANEL_CHILDREN}</Panel>
+      </Tabs>
+    );
+    const PanelElement = screen.getByText(SELECTOR_CHILDREN);
+    expect(PanelElement).toBeInTheDocument();
+    expect(PanelElement).toMatchSnapshot();
+  });
+});
+
+describe('useTabs hook', () => {
+
+  test('has to create a TabsContext', () => {
+    const setActiveTabMock = () => undefined;
+    const context = renderHook(() => useTabs(), {
+      initialProps: { value: { activeTab: 'TabA', setActiveTab: setActiveTabMock }},
+      wrapper: TabsContext.Provider,
+    });
+    expect(context.result).toStrictEqual({
+      current: {
+        activeTab: 'TabA',
+        setActiveTab: setActiveTabMock
+      },
+      error: undefined
+    });
+  });
+
+});
