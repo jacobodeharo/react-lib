1441dc176f0d087a50f7839b9f5125f09babb5a0
Jacobo de Haro <jacobodeharo@gmail.com>
2020-09-22 10:36:15 +0200

    Standardise export

--- src/core/ErrorBoundary/ErrorBoundary.tsx
+++ src/core/ErrorBoundary/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { ErrorBoundaryProps, ErrorBoundaryState } from './ErrorBoundary.types';
 
 export class ErrorBoundary extends React.PureComponent<
