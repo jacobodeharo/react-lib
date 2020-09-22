03c23b72c6d45fbb38ee704233f00915d4384d75
Jacobo de Haro <jacobodeharo@gmail.com>
2020-09-22 12:39:00 +0200

    Fix ESLint stacked (Ignore storybook-static folder) & update snapshots

--- src/components/Tabs/Panel/Panel.styles.tsx
+++ src/components/Tabs/Panel/Panel.styles.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const StyledPanel = styled.div`
-  background-color: var(--c-panel-bg, ${({ theme }) => theme.primaryColorLight});
+  background-color: var(
+    --c-panel-bg,
+    ${({ theme }) => theme.primaryColorLight}
+  );
   color: var(--c-panel-fg, ${({ theme }) => theme.primaryColorText});
   padding: 10px;
   border-radius: 0 0 5px 5px;
