f12c3e31081bb185a928bef46b180bf154c6506e
Jacobo de Haro <jacobodeharo@gmail.com>
2020-11-17 00:16:18 +0100

    Provide building of ESM and CommonJS modules

--- rollup.config.js
+++ rollup.config.js
@@ -11,66 +11,97 @@ const styledComponentsTransformer = createStyledComponentsTransformer();
 
 const lazyloadedComponents = ['Button', 'Anchor'];
 
-export default {
+const manualChunks = (id, { getModuleInfo }) => {
+  const regExp = new RegExp(
+    `/components/.*/(${lazyloadedComponents.join('|')})`
+  );
+  const match = regExp.exec(id);
+  if (match) {
+    const component = match[1]; // e.g. "Button"
+    const dependentEntryPoints = [];
+    // we use a Set here so we handle each module at most once. This
+    // prevents infinite loops in case of circular dependencies
+    const idsToHandle = new Set(getModuleInfo(id).dynamicImporters);
+    for (const moduleId of idsToHandle) {
+      const { isEntry, dynamicImporters, importers } = getModuleInfo(moduleId);
+      if (isEntry || dynamicImporters.length > 0) {
+        dependentEntryPoints.push(moduleId);
+      }
+      // The Set iterator is intelligent enough to iterate over elements that
+      // are added during iteration
+      for (const importerId of importers) {
+        idsToHandle.add(importerId);
+      }
+    }
+    // If there is a unique entry, we put it into into a chunk based on the entry name
+    if (dependentEntryPoints.length === 1) {
+      return `${
+        dependentEntryPoints[0].split('/').slice(-1)[0].split('.')[0]
+      }.${component}`;
+    }
+    // For multiple entries, we put it into a "shared" chunk
+    if (dependentEntryPoints.length > 1) {
+      return `shared.${component}`;
+    }
+  }
+};
+
+const plugins = [
+  peerDepsExternal(),
+  resolve(),
+  commonjs(),
+  typescript({
+    useTsconfigDeclarationDir: true,
+    transformers: [
+      () => ({
+        before: [styledComponentsTransformer],
+      }),
+    ],
+  }),
+  typescriptPaths({
+    absolute: true,
+  }),
+  postcss(),
+];
+
+const commonConfig = {
   input: 'src/index.ts',
   output: [
     {
       dir: 'build',
       format: 'esm',
       sourcemap: true,
+      entryFileNames: '[name].esm.js',
+      chunkFileNames: '[name].esm.js',
     },
   ],
-  manualChunks: (id, { getModuleInfo }) => {
-    const regExp = new RegExp(
-      `/components/.*/(${lazyloadedComponents.join('|')})`
-    );
-    const match = regExp.exec(id);
-    if (match) {
-      const component = match[1]; // e.g. "Button"
-      const dependentEntryPoints = [];
-      // we use a Set here so we handle each module at most once. This
-      // prevents infinite loops in case of circular dependencies
-      const idsToHandle = new Set(getModuleInfo(id).dynamicImporters);
-      for (const moduleId of idsToHandle) {
-        const { isEntry, dynamicImporters, importers } = getModuleInfo(
-          moduleId
-        );
-        if (isEntry || dynamicImporters.length > 0) {
-          dependentEntryPoints.push(moduleId);
-        }
-        // The Set iterator is intelligent enough to iterate over elements that
-        // are added during iteration
-        for (const importerId of importers) {
-          idsToHandle.add(importerId);
-        }
-      }
-      // If there is a unique entry, we put it into into a chunk based on the entry name
-      if (dependentEntryPoints.length === 1) {
-        return `${
-          dependentEntryPoints[0].split('/').slice(-1)[0].split('.')[0]
-        }.${component}`;
-      }
-      // For multiple entries, we put it into a "shared" chunk
-      if (dependentEntryPoints.length > 1) {
-        return `shared.${component}`;
-      }
-    }
-  },
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({
-      useTsconfigDeclarationDir: true,
-      transformers: [
-        () => ({
-          before: [styledComponentsTransformer],
-        }),
-      ],
-    }),
-    typescriptPaths({
-      absolute: true,
-    }),
-    postcss(),
-  ],
+  manualChunks,
+  plugins,
 };
+
+export default [
+  {
+    ...commonConfig,
+    output: [
+      {
+        dir: 'build',
+        format: 'esm',
+        sourcemap: true,
+        entryFileNames: '[name].esm.js',
+        chunkFileNames: '[name].esm.js',
+      },
+    ],
+  },
+  {
+    ...commonConfig,
+    output: [
+      {
+        dir: 'build',
+        format: 'cjs',
+        sourcemap: true,
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+      },
+    ],
+  },
+];
